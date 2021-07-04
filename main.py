be55fc45531261692546e55641e9abc1953e51c8
Joshua <83801098+Joshuah143@users.noreply.github.com>
2021-07-04 02:41:35 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,58 @@
+import base64
+import hashlib
+import sys
+
+from cryptography.fernet import Fernet, InvalidToken
+
+
+def encrypt(password: str = None, to_encrypt: str = None):
+    if password is None:
+        password = input('PASSWORD: ')
+    if to_encrypt is None:
+        to_encrypt = input('TO ENCRYPT: ')
+    result = Fernet(base64.urlsafe_b64encode(hashlib.sha3_512(password.encode())
+                                             .digest()[:32])).encrypt(bytes(to_encrypt.encode())).decode()
+    return [result, f'Token: {result}']
+
+
+def decrypt(password: str = None, to_decrypt: str = None):
+    if password is None:
+        password = input('PASSWORD: ')
+    if to_decrypt is None:
+        to_decrypt = input('TO DECRYPT: ')
+    while True:
+        try:
+            token = Fernet(base64.urlsafe_b64encode(hashlib.sha3_512(password.encode())
+                                                    .digest()[:32])).decrypt(bytes(to_decrypt.encode())).decode()
+            return [token, f'Message: {token}']
+        except InvalidToken:
+            print('INVALID TOKEN\nTry checking the password and the tokem')
+            continue
+
+
+if __name__ == '__main__':
+    print('Thank you for using my command line tool for message encryption. '
+          'Please remeber that ALL STRINGS ARE CASE SENSITIVE.\n'
+          'If you have an issue email me at joshua.himmens@icloud.com')
+    args = sys.argv
+    if 'decode' in args:
+        print(decrypt(args[args.index('decode') + 1], args[args.index('decode') + 2]))
+    elif 'encode' in args:
+        print(encrypt(args[args.index('encode') + 1], args[args.index('encode') + 2]))
+    else:
+        choice = input('DECODE OR ENCODE (encode/decode): ').upper()
+        if choice == 'DECODE':
+            print(decrypt()[1])
+        elif choice == 'ENCODE':
+            print(encrypt()[1])
+
+class ShortVersion:
+    @staticmethod
+    def encode(password: str, to_encrypt:str):
+        return Fernet(base64.urlsafe_b64encode(hashlib.sha3_512(password.encode())
+                                                 .digest()[:32])).encrypt(bytes(to_encrypt.encode())).decode()
+
+    @staticmethod
+    def decode(password: str, to_decrypt: str):
+        Fernet(base64.urlsafe_b64encode(hashlib.sha3_512(password.encode())
+                                        .digest()[:32])).decrypt(bytes(to_decrypt.encode())).decode()
